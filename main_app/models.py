a7a74401626a911bc4d285324224cd1b57f3d802
ampersand-paris <ampersand.s.paris@gmail.com>
2022-04-19 19:21:20 -0400

    task model created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+DAY_CHOICES = (
+    ("Mon", "Monday"),
+    ("Tue", "Tuesday"),
+    ("Wed", "Wednesday"),
+    ("Thurs", "Thursday"),
+    ("Fri", "Friday"),
+    ("Sat", "Saturday"),
+    ("Sun", "Sunday")
+)
+class Task(models.Model):
+
+    task = models.CharField(max_lenth=50)
+    description = models.CharField(max_length=250)
+    days = models.CharField(max_length=10, choices=DAY_CHOICES)
+    is_complete = models.BooleanField()
+
+    def __str__(self):
+        return self.task
+    
+    class Meta:
+        ordering = []
