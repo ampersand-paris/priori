739ae1b5d3abdfcbbaa2721357d09658231e61ce
ampersand-paris <ampersand.s.paris@gmail.com>
2022-04-20 22:17:59 -0400

    day url

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.Home.as_view(), name="todos"),
     path('login/', views.login_view, name="login"),
     path('accounts/signup/', views.signup_view, name="signup"),
-    path('profile/', views.Profile.as_view(), name="profile")
+    path('profile/', views.Profile.as_view(), name="profile"),
+    path('profile/days', views.Days.as_view(), name="days")
+
 ]
